27ca39447471b28e26b8cf6edbb66d104fe3b90d
Santam Kumai <santtamk@gmail.com>
2024-04-13 15:50:56 +0530

    final output

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 // validation for number input using bootstrap
 
 document.addEventListener("DOMContentLoaded", function () {
+  
   const form = document.getElementById("my-form");
+
+  const inputs = document.querySelectorAll(".form-control");
+  inputs.forEach((input) => {
+    input.addEventListener("input", function () {
+      if (input.value.trim() === "") {
+        hideError(input);
+      } else if (!isNumber(input.value)) {
+        showError(input);
+      } else {
+        hideError(input);
+      }
+    });
+  });
 
   form.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    const inputs = document.querySelectorAll(".form-control");
-    let isValid = true
+    let isValid = true;
+
     inputs.forEach((input) => {
-        if (this.value.trim() === "") {
-          // If the input value is empty, hide the error icon and tooltip
-          hideError(input)
-          isValid = false;
-        } else if (!isNumber(this.value)) {
-          showError(input)
-          isValid = false
-        } else {
-            hideError(input)
-        }
+      if (input.value.trim() === "" || !isNumber(input.value)) {
+        showError(input);
+        isValid = false;
+      } else {
+        hideError(input);
+      }
     });
 
-
-    if(isValid){
-        form.submit()
+    if (isValid) {
+      handleSubmission();
     }
   });
 
@@ -38,49 +47,63 @@ document.addEventListener("DOMContentLoaded", function () {
     errorTooltip.style.display = "block";
     input.classList.add("is-invalid");
   }
+
   function hideError(input) {
-    const errorIcon = this.parentElement.querySelector(".error-icon");
-    const errorTooltip = this.parentElement.querySelector(".error-tooltip");
+    const errorIcon = input.parentElement.querySelector(".error-icon");
+    const errorTooltip = input.parentElement.querySelector(".error-tooltip");
     errorIcon.classList.add("d-none");
     errorTooltip.style.display = "none";
-    this.classList.remove("is-invalid");
-  }
-});
-
-
-const grossAnnualIncomeInput = document.querySelector("#grossAnnualIncomeInput");
-const extraIncomeInput = document.querySelector("#extraIncomeInput");
-const applicableDeductionInput = document.querySelector("#applicableDeductionInput");
-const selectedAgeGroup = document.querySelector("#inputState");
-const submit = document.querySelector("#submit");
-
-submit.addEventListener("click", function (event) {
-  event.preventDefault();
-
-  const grossAnnualIncome = parseFloat(grossAnnualIncomeInput.value);
-  const extraIncome = parseFloat(extraIncomeInput.value);
-  const applicableDeduction = parseFloat(applicableDeductionInput.value);
-  const ageGroup = selectedAgeGroup.value;
-
-  let taxPercentage;
-  if (ageGroup === "<40") {
-    taxPercentage = 0.3;
-  } else if (ageGroup === "≥40 & <60") {
-    taxPercentage = 0.4;
-  } else if (ageGroup === "≥60") {
-    taxPercentage = 0.1;
+    input.classList.remove("is-invalid");
   }
 
-  const overallIncome = grossAnnualIncome + extraIncome - applicableDeduction;
-  console.log("overallIncome:", overallIncome);
-  console.log("deduction:", applicableDeduction);
-  let taxedIncome = overallIncome;
-  if (overallIncome > 800000) {
-    taxedIncome = overallIncome - taxPercentage * (overallIncome - 800000);
-  }
+  function handleSubmission() {
+    const grossAnnualIncomeInput = document.querySelector(
+      "#grossAnnualIncomeInput"
+    );
+    const extraIncomeInput = document.querySelector("#extraIncomeInput");
+    const applicableDeductionInput = document.querySelector(
+      "#applicableDeductionInput"
+    );
+    const selectedAgeGroup = document.querySelector("#inputState");
 
-  console.log(
-    "Overall Income after tax and deductions:",
-    taxedIncome.toFixed(2)
-  );
-});
+    const grossAnnualIncome = parseFloat(grossAnnualIncomeInput.value);
+    const extraIncome = parseFloat(extraIncomeInput.value);
+    const applicableDeduction = parseFloat(applicableDeductionInput.value);
+    const ageGroup = selectedAgeGroup.value;
+
+    let taxPercentage;
+    if (ageGroup === "<40") {
+      taxPercentage = 0.3;
+    } else if (ageGroup === "≥40 & <60") {
+      taxPercentage = 0.4;
+    } else if (ageGroup === "≥60") {
+      taxPercentage = 0.1;
+    }
+
+    const overallIncome = grossAnnualIncome + extraIncome - applicableDeduction;
+    console.log("overallIncome:", overallIncome);
+    console.log("deduction:", applicableDeduction);
+    let taxedIncome = overallIncome;
+    if (overallIncome > 800000) {
+      taxedIncome = overallIncome - taxPercentage * (overallIncome - 800000);
+    }
+
+    console.log(
+      "Overall Income after tax and deductions:",
+      taxedIncome.toFixed(2)
+    );
+
+    const overlay = document.getElementById('overlay')
+    overlay.classList.remove("d-none");
+    const reportText = document.getElementById('reportText')
+    reportText.innerText = 'Your overall income will be ₹' + taxedIncome.toFixed(2) + ' after tax deduction';
+    function openNav() {
+      document.querySelector(".finalOutputDisplay").style.width = "100%";
+    }
+    openNav()
+  }
+})
+function closeNav() {
+  document.querySelector(".finalOutputDisplay").style.width = "0%";
+}
+
